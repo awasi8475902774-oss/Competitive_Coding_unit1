ac6f966e3e07dcaf4491157b9c58892ad4554c8b
Abdul Wasi <a.wasi8475902774@gmail.com>
2026-08-04 12:00:45 +0530

    Time: 22 ms (93.77%), Space: 94.6 MB (26.26%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> seen = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!seen.add(nums[i])) {
+                return true;
+            }
+            
+            if (i >= k) {
+                seen.remove(nums[i - k]);
+            }
+        }
+        
+        return false;
+    }
+}
